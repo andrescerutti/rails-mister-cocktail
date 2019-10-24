ed5e3596075dbd6fe28b0d79949b240c22130143
andres cerutti <cerutti_andres@hotmail.com>
2019-10-24 18:41:26 -0300

    cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Cocktail.destroy_all
+
+puts 'Creating cocktails...'
+cocktails_attributes = [
+  {
+    name:         'fernet'
+  },
+  {
+    name:         'gin tonic'
+  },
+  {
+    name:         'aperol sprits'
+  },
+  {
+    name:         'mojito'
+  },
+  {
+    name:         'piel de higuana'
+  }
+]
+
+cocktails_attributes.each do |r|
+
+  rest = Cocktail.create!(r)
+
+end
+
+
+
+require 'open-uri'
+
+url = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients_array = JSON.parse(url)
+puts 'Creating ingredient...'
+ingredients_array['drinks'].each do |ingredient|
+  new_ingredient = Ingredient.new
+  new_ingredient.name = ingredient['strIngredient1']
+  new_ingredient.save
+end
+
+
+
+puts 'Finished!'
